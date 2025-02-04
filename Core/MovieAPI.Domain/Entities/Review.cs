0ab368e42d1acfde52870523513aee7ed432e128
SERVET AKCADAG <34627199+Albatros46@users.noreply.github.com>
2025-02-04 18:23:42 +0100

    1-Projenin oluşturulması ve Entity katmanının yazılması.

---
+++ Core/MovieAPI.Domain/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Domain.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string reviewComment { get; set; }
+        public int userRating { get; set; }
+        public DateTime reviewtDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
